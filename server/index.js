e03440b0f28f0d73722b1be68ed9d893952cf16b
rubypruneda <rubypruneda@outlook.com>
2018-08-17 20:34:01 -0600

    endpoints created

--- server/index.js
+++ server/index.js
@@ -4,6 +4,22 @@ const app = express()
 // const express = require('express-session')
 const { SERVER_PORT } = process.env;
 
+app.get('/info', (req, res) => {
+
+})
+
+app.delete('/delete', (req, res) => {
+
+})
+
+app.put('/update', (req, res) => {
+
+})
+
+app.post('/read', (req, res) => {
+    
+})
+
 app.listen(SERVER_PORT, () => {
     console.log(`here we go again on port ${SERVER_PORT}`)
 })
